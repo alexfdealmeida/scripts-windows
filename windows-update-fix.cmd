1abd345de2b00d45f40ad427428b166b5137113f
alex.almeida <alex.almeida@aliare.co>
2023-02-21 12:56:35 -0300

    Branch: main; windows-update-fix.cmd: Atualizada lista de comandos.

--- windows-update-fix.cmd
+++ windows-update-fix.cmd
@@ -1,7 +1,11 @@
+SC config wuauserv start=auto
+SC config cryptsvc start=auto
+SC config bits start=auto
+SC config trustedinstaller start=auto
+
 net stop wuauserv
 net stop cryptSvc
 net stop bits
-net stop msiserver
 net stop trustedinstaller
 
 ren C:\Windows\SoftwareDistribution SoftwareDistribution.old
@@ -10,7 +14,6 @@ ren C:\Windows\System32\catroot2 Catroot2.old
 net start wuauserv
 net start cryptSvc
 net start bits
-net start msiserver
 net start trustedinstaller
 
 dism /online /cleanup-image /restorehealth
