65835c4241a9905c9c0fdb75c3b94daf1cd958f4
alex.almeida <alex.almeida@aliare.co>
2025-12-03 13:01:18 -0300

    Branch: main; Renamed scripts.

--- scripts/policies/disable_policies_wallpaper.cmd
+++ scripts/policies/disable_policies_wallpaper.cmd
@@ -1,4 +1,4 @@
 rem cd %~dp0reg
-regedit /s "%~dp0reg\disable-policies-system-wallpaper.reg"
+regedit /s "%~dp0reg\disable_policies_system_wallpaper.reg"
 rem reg import disable-policies-system-wallpaper.reg
 rem pause
